f92b3639dc13f66918871f337bbbbcd9841e1b79
Christopher Eliasson <Christopher@northcube.com>
2017-09-06 13:18:17 +0200

    Added my version of the game, for reference

--- CardGame_WAR/PopUpViewController.swift
+++ CardGame_WAR/PopUpViewController.swift
@@ -11,37 +11,25 @@ import UIKit
 class PopUpViewController: UIViewController {
     
     @IBOutlet weak var winnerLabel: UILabel!
-    
-    var winner = "What? Who won? Was it a tie? Aargh!"
+
+    var gameResult: GameResult!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        winnerLabel.text = winner
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        guard let gameResult = gameResult else { return // Game result was not passed! 
+        }
+        
+        if gameResult.wasTied {
+            self.winnerLabel.text = "Aww, too bad! The game was tied, play again!"
+        
+        } else {
+            self.winnerLabel.text = self.gameResult.winner + " beat " + self.gameResult.loser + " with \(self.gameResult.winnerPoints) points to \(self.gameResult.loserPoints)"
+        }
     }
     
     @IBAction func restartGame(_ sender: UIButton) {
-        
-        dismiss(animated: true, completion: nil)
-        
+        self.dismiss(animated: true, completion: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- CardGame_WAR/ViewController.swift
+++ CardGame_WAR/ViewController.swift
@@ -8,112 +8,114 @@
 
 import UIKit
 
+public struct GameResult { // Struct used to store game results
+    let winner: String
+    let loser: String
+    let winnerPoints: Int
+    let loserPoints: Int
+    var wasTied: Bool = false
+}
+
 class ViewController: UIViewController {
     
-    let cardArray = [#imageLiteral(resourceName: "ace"), #imageLiteral(resourceName: "card2"), #imageLiteral(resourceName: "card3"), #imageLiteral(resourceName: "card4"), #imageLiteral(resourceName: "card5"), #imageLiteral(resourceName: "card6"), #imageLiteral(resourceName: "card7"), #imageLiteral(resourceName: "card8"), #imageLiteral(resourceName: "card9"), #imageLiteral(resourceName: "card10"), #imageLiteral(resourceName: "jack"), #imageLiteral(resourceName: "queen"), #imageLiteral(resourceName: "king")]
+    let cards = [#imageLiteral(resourceName: "ace"), #imageLiteral(resourceName: "card2"), #imageLiteral(resourceName: "card3"), #imageLiteral(resourceName: "card4"), #imageLiteral(resourceName: "card5"), #imageLiteral(resourceName: "card6"), #imageLiteral(resourceName: "card7"), #imageLiteral(resourceName: "card8"), #imageLiteral(resourceName: "card9"), #imageLiteral(resourceName: "card10"), #imageLiteral(resourceName: "jack"), #imageLiteral(resourceName: "queen"), #imageLiteral(resourceName: "king")]
     
     @IBOutlet weak var playerCard: UIImageView!
-    
     @IBOutlet weak var houseCard: UIImageView!
-    
     @IBOutlet weak var playerLabel: UILabel!
-    
     @IBOutlet weak var houseLabel: UILabel!
-    
     @IBOutlet weak var timerStackView: UIStackView!
-    
     @IBOutlet weak var timerLabel: UILabel!
     
     var playerScore: Int = 0
     var houseScore: Int = 0
-    var winner: String = ""
     
-    var seconds = 10
+    var gameTime: Int = 0 // in seconds
     var timer = Timer()
     var isTimerRunning = false
     
-    @IBAction func dealButtonPressed(_ sender: UIButton) {
-        runTimer()
-        
-        let playerNum = Int(arc4random_uniform(12))
-        let houseNum = Int(arc4random_uniform(12))
-        
-        playerCard.image = cardArray[playerNum]
-        houseCard.image = cardArray[houseNum]
-        
-        if playerNum > houseNum {
-            playerScore += 1
-            playerLabel.text = String(playerScore)
-        } else if houseNum > playerNum {
-            houseScore += 1
-            houseLabel.text = String(houseScore)
-        }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.resetGame() // The game will reset every time we enter this view.
     }
     
-    func runTimer() {
-        if isTimerRunning == false {
-            
-            timerStackView.alpha = 1
-            
-            isTimerRunning = true
-            
-            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+    fileprivate func runTimer() {
+        if !self.isTimerRunning {
+            self.timerStackView.alpha = 1
+            self.isTimerRunning = true
+            self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
         }
     }
     
     func updateTimer() {
-        if seconds > 0 {
-            seconds -= 1
-            timerLabel.text = "\(seconds)" + " seconds"
+        if self.gameTime > 0 {
+            self.gameTime -= 1
+            self.timerLabel.text = "\(gameTime)" + " seconds"
+            
         } else {
-            timer.invalidate()
             
-            timerStackView.alpha = 0
-            
-            if playerScore > houseScore {
-                winner = "Congratulations, you won!"
-            } else if houseScore > playerScore {
-                winner = "Aww, too bad... Better luck next time!!"
-            } else {
-                winner = "What? Who won? Was it a tie? Aargh!"
-            }
-            
-            self.performSegue(withIdentifier: "resultPopUp", sender: nil)
-            
-            seconds = 10
-            playerScore = 0
-            houseScore = 0
-            playerCard.image = #imageLiteral(resourceName: "back")
-            houseCard.image = #imageLiteral(resourceName: "back")
-            
-            isTimerRunning = false
-            
-            timerLabel.text = "10 seconds"
-            
-            playerLabel.text = "0"
-            houseLabel.text = "0"
+            // Game time is 0 thus game over
+            self.timer.invalidate()
+            self.timerStackView.alpha = 0
             
             
+            let gameResult = self.calculateWinner()
+            self.performSegue(withIdentifier: "resultPopUp", sender: gameResult)
         }
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let vc = segue.destination as? PopUpViewController, let winner = sender as? String {
-            vc.winner = winner
+    fileprivate func calculateWinner() -> GameResult {
+        // Set up game result struct where we store the results needed in PopUpViewController
+        if self.playerScore > self.houseScore {
+            return GameResult(winner: "Player", loser: "House", winnerPoints: self.playerScore, loserPoints: self.houseScore, wasTied: false)
+        } else if houseScore > playerScore {
+            return GameResult(winner: "House", loser: "Player", winnerPoints: self.houseScore, loserPoints: self.playerScore, wasTied: false)
+        } else {
+            return GameResult(winner: "", loser: "", winnerPoints: 0, loserPoints: 0, wasTied: true)
         }
     }
-
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    fileprivate func resetGame() {
+        self.gameTime = 10
+        self.playerScore = 0
+        self.houseScore = 0
+        self.playerCard.image = #imageLiteral(resourceName: "back")
+        self.houseCard.image = #imageLiteral(resourceName: "back")
+        self.isTimerRunning = false
+        self.timerLabel.text = "\(self.gameTime) seconds"
+        self.playerLabel.text = "0"
+        self.houseLabel.text = "0"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    // MARK: - Actions
+    
+    @IBAction func dealButtonPressed(_ sender: UIButton) {
+        
+        self.runTimer()
+        
+        let playerNum = Int(arc4random_uniform(12))
+        let houseNum = Int(arc4random_uniform(12))
+        
+        self.playerCard.image = self.cards[playerNum]
+        self.houseCard.image = self.cards[houseNum]
+        
+        if playerNum > houseNum {
+            self.playerScore += 1
+            self.playerLabel.text = String(self.playerScore)
+            
+        } else if houseNum > playerNum {
+            self.houseScore += 1
+            self.houseLabel.text = String(self.houseScore)
+        }
     }
-
-
+    
+    // MARK: - Segue
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? PopUpViewController, let gameResult = sender as? GameResult {
+            vc.gameResult = gameResult
+        }
+    }
 }
 
